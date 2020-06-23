82e12a5756a639d88709c67c9d6bf8d17218c803
Pausbrak <rslego@gmail.com>
2020-06-22 21:38:09 -0400

    don't show ranged DPS for unloaded mortars since it has no effect

--- Source/StatWorker_TurretRangedDPSBase.cs
+++ Source/StatWorker_TurretRangedDPSBase.cs
@@ -7,13 +7,27 @@ namespace RangedDPS
     {
         public override bool ShouldShowFor(StatRequest req)
         {
-            return req.Def is ThingDef thingDef && ThingDefIsShooty(thingDef?.building?.turretGunDef);
+            if (!(req.Def is ThingDef thingDef && ThingDefIsShooty(thingDef?.building?.turretGunDef)))
+            {
+                return false;
+            }
+
+            // Don't show DPS for unloaded mortars
+            var comp = GetTurretThing(req).TryGetComp<CompChangeableProjectile>();
+            if (comp != null)
+            {
+                return comp.Loaded;
+            }
+
+            return true;
         }
 
         protected static Thing GetTurretThing(StatRequest req)
         {
             if (req.Thing is Building_TurretGun turret)
+            {
                 return turret.gun;
+            }
             return (req.Def as ThingDef)?.building?.turretGunDef?.GetConcreteExample();
         }
     }

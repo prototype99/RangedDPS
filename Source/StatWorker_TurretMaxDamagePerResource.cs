ffab5451f9621f3dec16d788b1a7de11c898d2c8
Pausbrak <rslego@gmail.com>
2021-07-18 02:40:51 -0400

    Add another stat for maximum damage per resource

---
+++ Source/StatWorker_TurretMaxDamagePerResource.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RangedDPS.StatUtilities;
+using RimWorld;
+using Verse;
+
+namespace RangedDPS
+{
+
+    public class StatWorker_TurretMaxDamagePerResource : StatWorker_TurretDPSBase
+    {
+        public override bool ShouldShowFor(StatRequest req)
+        {
+            if (!base.ShouldShowFor(req)) return false;
+
+            // Don't show resource usage for turrets without fuel
+            return GetTurretStats(req).NeedsFuel;
+        }
+
+        public override float GetValueUnfinalized(StatRequest req, bool applyPostProcess = true)
+        {
+            if (!ShouldShowFor(req))
+            {
+                return 0f;
+            }
+
+            TurretStats turretStats = GetTurretStats(req);
+            return turretStats.DamagePerFuel;
+        }
+    }
+}
